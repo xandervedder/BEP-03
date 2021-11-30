75195da35ea96aaaf1a0e0a55c3a456471bbe066
Xander Vedder <xander.vedd@gmail.com>
2021-11-30 11:37:52 +0100

    Trigger all workflows

--- product/src/main/java/nl/softwarestrijders/waiter/product/ProductApplication.java
+++ product/src/main/java/nl/softwarestrijders/waiter/product/ProductApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
-
-    
 }
