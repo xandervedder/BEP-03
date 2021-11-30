5ea2f3076073a524816bafa5b08537c29fc5fbc6
Ruben van den Brink <60598779+Rubenvdbrink@users.noreply.github.com>
2021-11-30 12:24:06 +0100

    Setup continuous integration with github actions (#16)
    Co-authored-by: Xander Vedder <xander.vedd@gmail.com>

---
+++ review/src/main/java/nl/softwarestrijders/waiter/review/ReviewApplication.java
@@ -0,0 +1,11 @@
+package nl.softwarestrijders.waiter.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewApplication.class, args);
+    }
+}
