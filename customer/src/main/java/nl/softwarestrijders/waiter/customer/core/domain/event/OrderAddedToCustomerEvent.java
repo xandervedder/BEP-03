4aa1429731eb5995f4e523c49a8697e15da0e407
JustMilan <milandol321@gmail.com>
2021-12-16 13:41:57 +0100

    Remove unnessecary code

--- customer/src/main/java/nl/softwarestrijders/waiter/customer/core/domain/event/OrderAddedToCustomerEvent.java
+++ customer/src/main/java/nl/softwarestrijders/waiter/customer/core/domain/event/OrderAddedToCustomerEvent.java
@@ -1,15 +1,6 @@
 package nl.softwarestrijders.waiter.customer.core.domain.event;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.UUID;
 
-public record OrderAddedToCustomerEvent(UUID customerId, UUID orderId) implements CustomerDomainEvent {
-	private static final String ROUTING_KEY = "customer.order.added";
-
-	@Override
-	@JsonIgnore
-	public String key() {
-		return null;
-	}
+public record OrderAddedToCustomerEvent(UUID customerId, UUID orderId) {
 }
--- customer/src/main/java/nl/softwarestrijders/waiter/customer/core/domain/event/ReviewAddedToCustomerEvent.java
+++ customer/src/main/java/nl/softwarestrijders/waiter/customer/core/domain/event/ReviewAddedToCustomerEvent.java
@@ -1,15 +1,6 @@
 package nl.softwarestrijders.waiter.customer.core.domain.event;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.UUID;
 
-public record ReviewAddedToCustomerEvent(UUID customerId, UUID reviewId, String type) implements CustomerDomainEvent {
-	private static final String ROUTING_KEY = "customer.review.created";
-
-	@Override
-	@JsonIgnore
-	public String key() {
-		return ROUTING_KEY;
-	}
+public record ReviewAddedToCustomerEvent(UUID customerId, UUID reviewId, String type) {
 }
