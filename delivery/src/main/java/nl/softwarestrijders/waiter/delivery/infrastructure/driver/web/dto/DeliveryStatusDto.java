ac50a3589185243c588e3fafd4673e71fd270321
Arjen Norbart <arjennorbart@gmail.com>
2021-12-17 17:40:51 +0100

    Make delivery controller and dto

---
+++ delivery/src/main/java/nl/softwarestrijders/waiter/delivery/infrastructure/driver/web/dto/DeliveryStatusDto.java
@@ -0,0 +1,6 @@
+package nl.softwarestrijders.waiter.delivery.infrastructure.driver.web.dto;
+
+import nl.softwarestrijders.waiter.delivery.core.domain.Status;
+
+public record DeliveryStatusDto(Status status) {
+}
