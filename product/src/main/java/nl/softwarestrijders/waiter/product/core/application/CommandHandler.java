3adf6975d944c669ef5367d6c9854976968b8f53
unknown <rubenvandenbrink@hotmail.com>
2021-12-17 17:10:09 +0100

    Add new files

---
+++ product/src/main/java/nl/softwarestrijders/waiter/product/core/application/CommandHandler.java
@@ -0,0 +1,7 @@
+package nl.softwarestrijders.waiter.product.core.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommandHandler {
+}
---
+++ product/src/main/java/nl/softwarestrijders/waiter/product/core/presentation/ProductController.java
@@ -0,0 +1,7 @@
+package nl.softwarestrijders.waiter.product.core.presentation;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+}
