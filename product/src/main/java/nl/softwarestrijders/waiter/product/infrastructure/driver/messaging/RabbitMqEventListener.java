f2430267020c084daf0e14a70d5b187f27d29197
unknown <rubenvandenbrink@hotmail.com>
2021-12-19 14:50:15 +0100

    Add listeners for commands

--- product/src/main/java/nl/softwarestrijders/waiter/product/infrastructure/driver/messaging/RabbitMqEventListener.java
+++ product/src/main/java/nl/softwarestrijders/waiter/product/infrastructure/driver/messaging/RabbitMqEventListener.java
@@ -1,10 +1,29 @@
 package nl.softwarestrijders.waiter.product.infrastructure.driver.messaging;
 
+import nl.softwarestrijders.waiter.product.core.application.CommandHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 public class RabbitMqEventListener {
-    @RabbitListener(queues = "#{'${messaging.queue.product}'}")
-    public void listen(){} //ToDo: Add listener implementation.
+    private static final Logger LOGGER = Logger.getLogger(RabbitMqEventListener.class.getName());
+    private final CommandHandler commandHandler;
+
+    public RabbitMqEventListener(CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
+    }
+
+    @RabbitListener(queues = "#{'${messaging.queue.create-product}'}")
+    public void listen(CreateProductEvent event){
+        LOGGER.info("Received create product event: " + event);
+        this.commandHandler.handle(event);
+    }
+
+    @RabbitListener(queues = "#{'${messaging.queue.delete-product}'}")
+    public void listen(DeleteProductEvent event){
+        LOGGER.info("Received delete product event: " + event);
+        this.commandHandler.handle(event);
+    }
 }
