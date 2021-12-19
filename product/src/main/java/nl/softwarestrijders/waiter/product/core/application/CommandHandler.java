bf12c3b4d3c472d0aa56d89529e09684660407fe
unknown <rubenvandenbrink@hotmail.com>
2021-12-19 14:30:28 +0100

    Add add product command and functionality

--- product/src/main/java/nl/softwarestrijders/waiter/product/core/application/CommandHandler.java
+++ product/src/main/java/nl/softwarestrijders/waiter/product/core/application/CommandHandler.java
@@ -1,7 +1,37 @@
 package nl.softwarestrijders.waiter.product.core.application;
 
+import nl.softwarestrijders.waiter.product.core.application.exceptions.ProductAlreadyExistsException;
+import nl.softwarestrijders.waiter.product.core.domain.NutritionalValue;
+import nl.softwarestrijders.waiter.product.core.domain.Product;
+import nl.softwarestrijders.waiter.product.core.port.data.ProductRepository;
+import nl.softwarestrijders.waiter.product.infrastructure.driver.messaging.CreateProductEvent;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommandHandler {
+    private final ProductRepository repository;
+
+    public CommandHandler(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void handle(CreateProductEvent event) {
+        if(repository.existByPriceAndNameAndDescription(event.price(), event.name(), event.description())) {
+            throw new ProductAlreadyExistsException(event.name());
+        }
+        repository.save(
+                new Product(
+                event.price(),
+                event.name(),
+                event.description(),
+                event.weight(),
+                new NutritionalValue(
+                        event.kcal(),
+                        event.fats(),
+                        event.carbs(),
+                        event.proteins(),
+                        event.salts()
+                )
+        ));
+    }
 }
--- product/src/main/java/nl/softwarestrijders/waiter/product/core/port/data/ProductRepository.java
+++ product/src/main/java/nl/softwarestrijders/waiter/product/core/port/data/ProductRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.UUID;
 
 public interface ProductRepository extends MongoRepository<Product, UUID> {
+    boolean existByPriceAndNameAndDescription(double price, String name, String description);
 }
