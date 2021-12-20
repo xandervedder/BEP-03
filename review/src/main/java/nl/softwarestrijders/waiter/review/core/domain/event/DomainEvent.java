55633a800bf14b8fc38235dd824842fff441b198
Xander Vedder <xander.vedd@gmail.com>
2021-12-20 15:53:58 +0100

    Update documentation of DomainEvent

--- review/src/main/java/nl/softwarestrijders/waiter/review/core/domain/event/DomainEvent.java
+++ review/src/main/java/nl/softwarestrijders/waiter/review/core/domain/event/DomainEvent.java
@@ -2,8 +2,7 @@ package nl.softwarestrijders.waiter.review.core.domain.event;
 
 public sealed interface DomainEvent permits ReviewCreatedEvent, ReviewDeletedEvent {
     /**
-     * The key of the event. This is only used for routing the message.
-     * TODO: Do we want to change this when we want to send to multiple destinations?
+     * The key of the event. This is only used for routing the event.
      *
      * @return String
      */
