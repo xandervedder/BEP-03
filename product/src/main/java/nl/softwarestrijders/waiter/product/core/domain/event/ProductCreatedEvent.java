9d778968ace10c318b0b043a6cb6808137448326
unknown <rubenvandenbrink@hotmail.com>
2021-12-20 16:46:25 +0100

    Add new events

---
+++ product/src/main/java/nl/softwarestrijders/waiter/product/core/domain/event/ProductCreatedEvent.java
@@ -0,0 +1,12 @@
+package nl.softwarestrijders.waiter.product.core.domain.event;
+
+import nl.softwarestrijders.waiter.product.core.domain.Product;
+
+public record ProductCreatedEvent(Product product) implements ProductEvent{
+    private static final String ROUTING_KEY = "events.product.created";
+
+    @Override
+    public String getRoutingKey() {
+        return ROUTING_KEY;
+    }
+}
