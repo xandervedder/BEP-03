6b6fcdba6a3837863483fd0be2681eb160ac0018
unknown <rubenvandenbrink@hotmail.com>
2021-12-20 09:30:21 +0100

    Make code more uniform

--- product/src/main/java/nl/softwarestrijders/waiter/product/infrastructure/driver/web/ProductDto.java
+++ product/src/main/java/nl/softwarestrijders/waiter/product/infrastructure/driver/web/ProductDto.java
@@ -2,10 +2,12 @@ package nl.softwarestrijders.waiter.product.infrastructure.driver.web;
 
 import java.util.UUID;
 
-public record ProductDto(UUID id,
-                         double price,
-                         String name,
-                         String description,
-                         int weight,
-                         NutritionalValueDto nutritionalValueDto) {
+public record ProductDto(
+        UUID id,
+        double price,
+        String name,
+        String description,
+        int weight,
+        NutritionalValueDto nutritionalValueDto
+) {
 }
