56783e77ec56ea989cee9cdc4c56de36d374154f
JustMilan <milandol321@gmail.com>
2021-12-21 13:42:59 +0100

    Add endpoints for deliveries, orders and reviews

--- customer/src/main/java/nl/softwarestrijders/waiter/customer/infrastructure/driver/web/CustomerController.java
+++ customer/src/main/java/nl/softwarestrijders/waiter/customer/infrastructure/driver/web/CustomerController.java
@@ -3,12 +3,16 @@ package nl.softwarestrijders.waiter.customer.infrastructure.driver.web;
 import nl.softwarestrijders.waiter.customer.core.application.CustomerCommandHandler;
 import nl.softwarestrijders.waiter.customer.core.application.CustomerQueryHandler;
 import nl.softwarestrijders.waiter.customer.core.application.command.RegisterCustomer;
+import nl.softwarestrijders.waiter.customer.core.application.query.GetDeliveriesFromCustomer;
+import nl.softwarestrijders.waiter.customer.core.application.query.GetOrdersFromCustomer;
+import nl.softwarestrijders.waiter.customer.core.application.query.GetReviewsFromCustomer;
 import nl.softwarestrijders.waiter.customer.core.domain.Customer;
 import nl.softwarestrijders.waiter.customer.infrastructure.driver.web.request.RegisterCustomerRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/customer")
@@ -35,5 +39,20 @@ public class CustomerController {
 						request.city()
 				)
 		);
+	}
+
+	@GetMapping("/reviews")
+	public Map<UUID, String> getReviewsFromCustomer(@RequestBody UUID customerId) {
+		return this.queryHandler.handle(new GetReviewsFromCustomer(customerId));
+	}
+
+	@GetMapping("/orders")
+	public List<UUID> getOrdersFromCustomer(@RequestBody UUID customerId) {
+		return this.queryHandler.handle(new GetOrdersFromCustomer(customerId));
+	}
+
+	@GetMapping("/deliveries")
+	public List<UUID> getDeliveriesFromCustomer(@RequestBody UUID customerId) {
+		return this.queryHandler.handle(new GetDeliveriesFromCustomer(customerId));
 	}
 }
