03bb669f3b97ba0204b9254e9c26427995d91165
Xander Vedder <xander.vedd@gmail.com>
2021-12-21 11:42:30 +0100

    Update DeleteReviewDto

--- review/src/main/java/nl/softwarestrijders/waiter/review/infrastructure/driver/web/dto/DeleteReviewDto.java
+++ review/src/main/java/nl/softwarestrijders/waiter/review/infrastructure/driver/web/dto/DeleteReviewDto.java
@@ -2,5 +2,5 @@ package nl.softwarestrijders.waiter.review.infrastructure.driver.web.dto;
 
 import java.util.UUID;
 
-public record DeleteReviewDto(UUID customerId, UUID reviewId) {
+public record DeleteReviewDto(UUID customerId) {
 }
