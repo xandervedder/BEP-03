f9665065474b2f0eddf1628aabf0615aad5f5e5e
Xander Vedder <xander.vedd@gmail.com>
2021-12-23 11:45:00 +0100

    Remove unused outgoing events

--- customer/src/main/java/nl/softwarestrijders/waiter/customer/core/domain/event/ReviewAddedToCustomerEvent.java
+++
@@ -1,6 +0,0 @@
-package nl.softwarestrijders.waiter.customer.core.domain.event;
-
-import java.util.UUID;
-
-public record ReviewAddedToCustomerEvent(UUID customerId, UUID reviewId, String type) {
-}
